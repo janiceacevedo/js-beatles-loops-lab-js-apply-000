576f479f1b6810b6e4df32409d58d9ee7fa6d29a
blithe-debugger-5623 <janiceacevedo1@gmail.com>
2020-09-21 22:17:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["bass", "guitar", "drums"];
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  var facts = ["John Lennon: rhythm guitar", "Paul McCartney: bass", "George Harrison: lead guitar", "Ringo Starr: drums"];
+  for (i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + "plays" + instruments[i]);
+  }
+}
+
+function johnLennonFacts() {
+  var facts = [("He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice")];
+  while (facts.length) {
+  }
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    ("I love the Beatles!");
+  } while (number < 15);
+}
